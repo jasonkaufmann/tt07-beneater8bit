fed09962a0527db175edb2f63e32b9a74da5ebcd
Jason Kaufmann <41923667+jasonkaufmann@users.noreply.github.com>
2024-05-01 13:34:17 -0700

    Update project.sv

--- src/project.sv
+++ src/project.sv
@@ -5,7 +5,7 @@
 
 `default_nettype none
 `include "eightBit.sv"
-module eater_8bit (
+module tt_um_eater_8bit (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
