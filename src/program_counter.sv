c6c289035e8f10f24a1bb57fb021f4be32819ace
Jason Kaufmann <jakaufmann@tesla.com>
2024-05-06 17:29:29 -0700

    now everything should be initialized to 0?

--- src/program_counter.sv
+++ src/program_counter.sv
@@ -8,14 +8,12 @@ module programCounter (
     output reg [3:0] addr = 0
 );
 
-    always @ (posedge clk) begin
-        if(rst == 1'b1) begin
+    always @ (posedge clk or negedge rst) begin
+        if(!rst) begin
             addr <= 0;
-        end
-        else if(countEnable == 1'b1) begin
+        end else if(countEnable == 1'b1) begin
             addr <= addr + 1;
-        end
-        else if (jump == 1'b1) begin
+        end else if (jump == 1'b1) begin
             addr <= jumpAddr;
         end
     end
